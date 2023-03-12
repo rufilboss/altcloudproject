6b265dc6000e8885e3800d19e9c8a3c798c3a6ed
Ilyas Rufai <rufaiilyas@gmail.com>
2023-03-12 22:39:35 +0100

    feat: add some code for deployment

--- Terraform/eks.tf
+++ Terraform/eks.tf
@@ -1,25 +1,31 @@
-resource "aws_eks_cluster" "cluster" {
-  name     = "my-k8s-cluster"
-  role_arn = aws_iam_role.cluster.arn
+resource "aws_eks_cluster" "aws_eks" {
+  name     = "eks_cluster_voting_app"
+  role_arn = aws_iam_role.eks_cluster.arn
 
   vpc_config {
-    subnet_ids = aws_subnet.private.*.id
+    subnet_ids = var.default_vpc_subnets
+  }
+
+  tags = {
+    Name = "EKS_Voting_App_Stack"
+  }
+}
+
+resource "aws_eks_node_group" "node" {
+  cluster_name    = aws_eks_cluster.aws_eks.name
+  node_group_name = "node-group-1"
+  node_role_arn   = aws_iam_role.eks_nodes.arn
+  subnet_ids      = var.default_vpc_subnets
+
+  scaling_config {
+    desired_size = 1
+    max_size     = 1
+    min_size     = 1
   }
 
   depends_on = [
-    aws_iam_role_policy_attachment.eks,
+    aws_iam_role_policy_attachment.AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.AmazonEC2ContainerRegistryReadOnly,
   ]
 }
-/* =========== */
-/* resource "aws_eks_cluster" "cluster" {
- name = "my-k8s-cluster"
- role_arn = aws_iam_role.eks-iam-role.arn
-
- vpc_config {
-  subnet_ids = [var.subnet_id_1, var.subnet_id_2]
- }
-
- depends_on = [
-  aws_iam_role.eks-iam-role,
- ]
-} */
